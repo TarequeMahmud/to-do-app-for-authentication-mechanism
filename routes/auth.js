9139b79671acc3294412cde929688bc1e67364c2
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-12-24 14:17:52 +0600

    feat: add logout functionality

--- routes/auth.js
+++ routes/auth.js
@@ -68,4 +68,11 @@ router.post(
   })
 );
 
+router.post("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    res.redirect("/");
+  });
+});
+
 module.exports = router;
