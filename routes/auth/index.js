965af1ff10f1d49ce346a98c27dc970f750624d0
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-12-27 10:52:50 +0600

    fix: fix critical problems with google authentication.

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,22 +1,142 @@
 var express = require("express");
-var router = express.Router();
-var localAuth = require("./strategies/localAuth");
-var googleAuth = require("./strategies/googleAuth");
 var crypto = require("crypto");
 var db = require("../../db");
+var GoogleStrategy = require("passport-google-oidc");
+var LocalStrategy = require("passport-local");
+
 const passport = require("passport");
 
+passport.use(
+  new LocalStrategy(function verify(username, password, cb) {
+    db.get(
+      "SELECT * FROM users WHERE username = ?",
+      [username],
+      function (err, row) {
+        if (err) {
+          return cb(err);
+        }
+        if (!row) {
+          return cb(null, false, {
+            message: "Incorrect username or password.",
+          });
+        }
+
+        crypto.pbkdf2(
+          password,
+          row.salt,
+          310000,
+          32,
+          "sha256",
+          function (err, hashedPassword) {
+            if (err) {
+              return cb(err);
+            }
+            if (!crypto.timingSafeEqual(row.hashed_password, hashedPassword)) {
+              return cb(null, false, {
+                message: "Incorrect username or password.",
+              });
+            }
+            return cb(null, row);
+          }
+        );
+      }
+    );
+  })
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env["GOOGLE_CLIENT_ID"],
+      clientSecret: process.env["GOOGLE_CLIENT_SECRET"],
+      callbackURL: "/oauth2/redirect/google",
+      scope: ["profile"],
+    },
+    function verify(issuer, profile, cb) {
+      db.get(
+        "SELECT * FROM federated_credentials WHERE provider = ? AND subject = ?",
+        [issuer, profile.id],
+        function (err, row) {
+          if (err) {
+            return cb(err);
+          }
+          if (!row) {
+            db.run(
+              "INSERT INTO users (name) VALUES (?)",
+              [profile.displayName],
+              function (err) {
+                if (err) {
+                  return cb(err);
+                }
+
+                var id = this.lastID;
+                db.run(
+                  "INSERT INTO federated_credentials (user_id, provider, subject) VALUES (?, ?, ?)",
+                  [id, issuer, profile.id],
+                  function (err) {
+                    if (err) {
+                      return cb(err);
+                    }
+                    var user = {
+                      id: id,
+                      name: profile.displayName,
+                    };
+                    return cb(null, user);
+                  }
+                );
+              }
+            );
+          } else {
+            db.get(
+              "SELECT * FROM users WHERE id = ?",
+              [row.user_id],
+              function (err, row) {
+                if (err) {
+                  return cb(err);
+                }
+                if (!row) {
+                  return cb(null, false);
+                }
+                return cb(null, row);
+              }
+            );
+          }
+        }
+      );
+    }
+  )
+);
+
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    cb(null, { id: user.id, username: user.username, name: user.name });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+
+var router = express.Router();
 router.get("/login", function (req, res, next) {
   res.render("login");
 });
 
-router.use("/login/password", localAuth);
-router.use("/login/federated/google", googleAuth);
+router.use(
+  "/login/password",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+router.use("/login/federated/google", passport.authenticate("google"));
 router.get(
   "/oauth2/redirect/google",
   passport.authenticate("google", {
-    successRedirect: "/",
     failureRedirect: "/login",
+    successRedirect: "/",
   })
 );
 router.post("/logout", (req, res, next) => {
